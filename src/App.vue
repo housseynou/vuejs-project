b8b5242ebe928c610009c3faee574d90981387bc
housseynou coulibaly <housseynou7@gmail.com>
2021-05-27 20:34:36 +0200

    backgtound almost done

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,25 @@
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
+  background-image: url(./assets/sky-surface.jpg);
+  background-size: cover;
+  background-position: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
+main {
+  min-height: 100vh;
+}
 #nav {
   padding: 30px;
 
