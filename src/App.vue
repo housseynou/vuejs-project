95f061a346a2a8ceb1ca0ab5e225cfcfdb98fdb2
housseynou coulibaly <housseynou7@gmail.com>
2021-05-27 20:48:47 +0200

    background done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <main>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </main>
   </div>
   <router-view/>
 </template>
