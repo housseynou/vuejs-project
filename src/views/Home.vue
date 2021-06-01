c32727d390ca7668758561db6ed4e7bcecc4b170
housseynou coulibaly <housseynou7@gmail.com>
2021-06-01 15:21:36 +0200

    home page design good

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,17 @@
     <div class="search-box">
          <i class="fas fa-search"></i><input type="text" class="search">
     </div>
-    <h1>this the home page</h1>
+    <HelloWorld msg="Météo Du Monde"/>
+    <div class="info">Utilisez la barre de recherche pour connaitre <br> le temps qu'il fait dans votre ville</div>
+    <div class="date">Lundi 01/06/2021</div>
+    <div class="ville">Bruxelles, Belgium</div>
+    <div class="meteo">
+      <div class="temperature">24°c</div>
+      <div class="temps">Soleil</div>
+    </div>
     
     
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
@@ -23,6 +30,59 @@ export default {
 </script>
 
 <style>
+  .hello {
+    width: 320px;
+    margin: 20px auto;
+    align-self: center;
+    color: rgb(62, 136, 20);
+    border: 1px solid rgb(62, 136, 20);
+  }
+  .info {
+    font-size: 18px;
+    font-weight: bold;
+    color: rgb(47, 95, 19);
+  }
+  .date {
+    margin-top: 22px;
+    font-weight: bold;
+    color: rgb(39, 68, 22);
+  }
+  .ville {
+    margin: auto;
+    width: 280px;
+    height: 50px;
+    border-radius: 15px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 30px;
+    background-color: rgba(255, 255, 255, 0.2);
+    color: rgb(13, 241, 192);
+    font-family: 'Times New Roman', Times, serif;
+    font-weight: bold;
+    box-shadow: 2px 3px rgba(0, 0, 0, 0.1);
+  }
+  .meteo {
+    margin: 20px auto;
+    width: 250px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 20px;
+    box-shadow: 3px 6px rgba(0, 0, 0, 0.1);
+  }
+  .temperature {
+    font-size: 95px;
+    font-weight: 800;
+    color: whitesmoke;
+  }
+  .temps {
+    font-size: 30px;
+    font-weight: 700;
+    color: rgb(200, 252, 247);
+  }
   .search-box {
     position: relative;
     bottom: 60px;
